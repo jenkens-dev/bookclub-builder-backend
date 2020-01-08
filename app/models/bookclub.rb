9555aae01bfefb82ed4302a6e38935a2cbf53c0b
Taljjaa <jenken131@gmail.com>
2020-01-08 14:24:36 -0800

    added associations to models, uncommented gems and cors file, and migrated tables

--- app/models/bookclub.rb
+++ app/models/bookclub.rb
@@ -1,2 +1,5 @@
 class Bookclub < ApplicationRecord
+    has_many :users
+    has_many :bookclub_books
+    has_many :books, through: :bookclub_books
 end
--- app/models/bookclub_book.rb
+++ app/models/bookclub_book.rb
@@ -1,2 +1,4 @@
 class BookclubBook < ApplicationRecord
+    belongs_to :book 
+    belongs_to :bookclub
 end
