c85177ba3208618f216c5597cec7d40841189fe6
Taljjaa <jenken131@gmail.com>
2020-01-14 10:32:32 -0800

    added update action to bookclubs controller

--- app/controllers/api/v1/bookclubs_controller.rb
+++ app/controllers/api/v1/bookclubs_controller.rb
@@ -12,6 +12,15 @@ class Api::V1::BookclubsController < ApplicationController
     ]
     end
 
+    def update 
+        @bookclub = Bookclub.find(params[:id])
+        @bookclub.update(name: params[:name], description: params[:description], picture: params[:picture])
+        render :json => @bookclub, :include => [
+            :users,
+            :bookclub_users => {:only => :is_admin}
+    ]
+    end
+
     def shuffle_books
         @bookclubs = Bookclub.all.to_a.shuffle
         @bookclubs = @bookclubs.slice(0,6)
