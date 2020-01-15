f2a1c84b79f32a373e8dcb7722b155ed0292ed24
Taljjaa <jenken131@gmail.com>
2020-01-15 10:05:08 -0800

    added create poll action to controller and it successfully returns a poll object

--- app/controllers/api/v1/polls_controller.rb
+++ app/controllers/api/v1/polls_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::PollsController < ApplicationController
+    def create
+        @poll = Poll.create(name: params[:name], bookclub_id: params[:bookclub_id])
+        render json: @poll
+    end
 end
